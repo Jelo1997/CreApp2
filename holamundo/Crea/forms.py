de0e337a29117da46dc2d598533a772239651533
miguelv1261 <99453270+miguelv1261@users.noreply.github.com>
2024-02-18 20:09:26 -0500

    Update forms.py

--- holamundo/Crea/forms.py
+++ holamundo/Crea/forms.py
@@ -69,3 +69,9 @@ class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ['nombre', 'apellido', 'cedula', 'telefono', 'correo', 'observaciones']
+
+class EmpleadoForm(forms.ModelForm):
+
+    class Meta:
+        model = Empleado
+        fields = ['telefono', 'apellido','area', 'correo', 'nombre']
