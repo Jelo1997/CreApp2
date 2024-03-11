0e87efec194a58e600ec5e1aa2eb083501e16e9a
miguelv1261 <99453270+miguelv1261@users.noreply.github.com>
2024-03-11 14:28:38 -0500

    Update views.py

--- holamundo/Crea/views.py
+++ holamundo/Crea/views.py
@@ -480,14 +480,14 @@ def agregar_observaciones(request, cliente_id):
 def captar_propiedad2(request, propiedad_id):
     propiedad = Propiedad_disponible.objects.get(id=propiedad_id)
     if request.method == 'POST':
-        form = CapturarPropiedadForm(request.POST)
+        form = CapturarProcesoForm(request.POST)
         if form.is_valid():
             proceso = form.save(commit=False)
             proceso.id_propiedad = propiedad
             proceso.save()
             return redirect('captarpro', propiedad_id=propiedad_id)
     else:
-        form = CapturarPropiedadForm(initial={'id_propiedad': propiedad})
+        form = CapturarProcesoForm(initial={'id_propiedad': propiedad})
     clientes = Cliente.objects.all()  # Asegúrate de importar Cliente y Empleado
     empleados = Empleado.objects.all()
     return render(request, 'detalle_propiedaddis.html', {'form': form, 'propiedad': propiedad, 'clientes': clientes, 'empleados': empleados, 'propiedad_id': propiedad_id})
