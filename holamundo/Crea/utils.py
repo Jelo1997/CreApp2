583f1091efadb9dbde37b63f637088f07caaf6bb
miguelv1261 <99453270+miguelv1261@users.noreply.github.com>
2024-03-10 23:29:01 -0500

    Update utils.py

--- holamundo/Crea/utils.py
+++ holamundo/Crea/utils.py
@@ -7,7 +7,9 @@ from holamundo.settings import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_WHA
 def enviar_mensaje_whatsapp(destinatario, mensaje):
     try:
         # Inicializa el cliente de Twilio con tus credenciales
-        client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+        account_sid = 'AC5e2d44bfed136b0af07958a7b6a1b36c'
+        auth_token = '6ef75c578ab24a6a2ba5699e81ea7f6b'
+        client = Client(account_sid, auth_token)
         
         # Crea el mensaje de WhatsApp
         message = client.messages.create(
@@ -17,7 +19,7 @@ def enviar_mensaje_whatsapp(destinatario, mensaje):
         )
         
         # Retorna el SID del mensaje si se envía correctamente
-        return message.sid
+        print(message.sid) 
     except Exception as e:
         # Maneja cualquier excepción que pueda ocurrir durante el envío del mensaje
         print(f"Error al enviar mensaje de WhatsApp: {str(e)}")
