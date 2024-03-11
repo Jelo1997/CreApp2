eabad63f4e99abf76439c8839294927dd549f0f5
miguelv1261 <99453270+miguelv1261@users.noreply.github.com>
2024-03-11 11:31:34 -0500

    Update views.py

--- holamundo/Crea/views.py
+++ holamundo/Crea/views.py
@@ -473,19 +473,17 @@ def captar_propiedad2(request, propiedad_id):
     if request.method == 'POST':
         form = CapturarPropiedadForm(request.POST)
         if form.is_valid():
-            proceso = form.save(commit=False)  # Obtener una instancia del proceso sin guardar en la base de datos
-            proceso.id_propiedad = propiedad  # Asignar la propiedad relacionada al proceso
-            proceso.save()  # Guardar el proceso en la base de datos
-            return redirect(reverse('detalle_propiedaddis', args=[propiedad_id]))  # Redirigir a la página de éxito después de guardar la propiedad
+            proceso = form.save(commit=False)
+            proceso.id_propiedad = propiedad
+            proceso.save()
+            # Redirect to captarpro view after successful form submission
+            return redirect('captarpro', propiedad_id=propiedad_id)
     else:
         form = CapturarPropiedadForm()
-    # Obtener todos los clientes y empleados
     clientes = Cliente.objects.all()
     empleados = Empleado.objects.all()
-    print(proceso)
-    print(propiedad)
-    return render(request, 'detalle_propiedaddis.html', {'form': form, 'propiedad': propiedad, 'clientes': clientes, 'empleados': empleados,'propiedad_id': propiedad_id})
+    return render(request, 'detalle_propiedaddis.html', {'form': form, 'propiedad': propiedad, 'clientes': clientes, 'empleados': empleados, 'propiedad_id': propiedad_id})
 
 def captarpro_view(request, propiedad_id):
-    # Any additional context you want to pass to captarpro.html
+    # Render captarpro.html with the provided propiedad_id
     return render(request, 'captarpro.html', {'propiedad_id': propiedad_id})
